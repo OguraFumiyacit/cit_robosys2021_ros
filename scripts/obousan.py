6edc3a421a9e65d3e0a7756b1a726719b461f8b8
OguraFumiyacit <s20C1029ua@s.chibakoudai.jp>
2021-12-14 04:30:54 +0000

    update

--- scripts/obousan.py
+++ scripts/obousan.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
 import rospy
 #from std_msgs.msg import String
-from std_msgs.msg import Int16
+from std_msgs.msg import Int32
 def cb(message):
     #rospy.loginfo(message.data)
     f = open('kyoten.txt', 'r')
-    if message.data == 0:
-        str = f.readlines()[0]
-        print(str)
-    elif message.data == 1:
-        str = f.readlines()[1]
-        print(str)
+    global n
+    n = message.data
+    str = f.readlines()[n]
+    print(str)
+    
+    #elif message.data == 1:
+        #str = f.readlines()[1]
+        #print(str)
 
 
 if __name__ == '__main__':
     rospy.init_node('obousan')
     #sub = rospy.Subscriber('input_data', String, cb)
-    sub = rospy.Subscriber('input_data', Int16, cb)
+    sub = rospy.Subscriber('input_data', Int32, cb)
     rospy.spin()
--- scripts/okyou.py
+++ scripts/okyou.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 import rospy
 #from std_msgs.msg import String
-from std_msgs.msg import Int16
+from std_msgs.msg import Int32
 
 
 rospy.init_node('okyou')
 #pub = rospy.Publisher('input_data', String, queue_size=1)
-pub = rospy.Publisher('input_data',Int16, queue_size=10)
+pub = rospy.Publisher('input_data',Int32, queue_size=1)
 rate = rospy.Rate(1)
 n = 0
 
@@ -17,9 +17,10 @@ while not rospy.is_shutdown():
     #    break
     #pub.publish(str.rstrip('\n'))
     #f.close()
-    msg = Int16()
+    msg = Int32()
     msg.data = n
     pub.publish(n)
     n += 1
-    if n >= 10:
+    rate.sleep()
+    if n >= 17:
         n = 0
