efbe9dc868ab30b7494f746aec724411b28256b8
OguraFumiyacit <s20C1029ua@s.chibakoudai.jp>
2021-12-18 01:48:20 +0000

    update

---
+++ scripts/reader1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import rospy
+from std_msgs.msg import String
+
+
+def cb(message):
+    rospy.loginfo(message.data)
+    #f = open('scripture.txt', 'r')
+    #global n
+    #n = message.data
+    #str = f.readlines()[n]
+    #print(str)
+
+
+if __name__ == '__main__':
+    rospy.init_node('reader')
+    sub = rospy.Subscriber('count_up', String, cb)
+    rospy.spin()
--- scripts/talker1.py
+++ scripts/talker1.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
 import rospy
-from std_msgs.msg import Int32
+from std_msgs.msg import String
 
 rospy.init_node('talker')
-pub = rospy.Publisher('count_up',Int32, queue_size=1)
+pub = rospy.Publisher('count_up',String, queue_size=1)
 rate = rospy.Rate(1)
 n = 0
+
+
 while not rospy.is_shutdown():
-    msg = Int32()
-    msg.data = n
-    pub.publish(n)
     count = 0
-    n +=1
     with open('scripture.txt') as f:
         for line in f:
             count += 1
+    with open('scripture.txt') as f:
+        str = f.readlines()[n-1]
     if n >= count:
         n = 0
-    print(count)
+    n += 1
+    pub.publish(str.rstrip('\n'))
     rate.sleep()
+
