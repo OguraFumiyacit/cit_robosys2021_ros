4e6e1ca340f84c1bcaa451c787952f23f8c4d55f
OguraFumiyacit <91648413+OguraFumiyacit@users.noreply.github.com>
2021-12-19 09:29:43 +0900

    Update talker.py

--- scripts/talker.py
+++ scripts/talker.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (C) 2021 Fumiya Ogura, Ryuichi ueda. All rights reserved.
+
 import rospy
 from std_msgs.msg import String
 
